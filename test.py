536cb1657d8bdd8f8f4d1edbb859b19a7093b717
sushantfaujdar <sushantf21@iitk.ac.in>
2024-02-28 21:41:34 +0530

    Model added

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import cv2
 import pickle
 import numpy as np
 import os
+import csv
+import time
+from datetime import datetime
+
+
 video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
@@ -11,10 +16,10 @@ with open('data/names.pkl', 'rb') as f:
 with open('data/faces_data.pkl', 'rb') as f:
         FACES=pickle.load(f)
 
-Knn=KNeighborsClassifier(n_neighbors=10)
+Knn=KNeighborsClassifier(n_neighbors=5)
 Knn.fit(FACES, LABELS)
 
-imgBackground=cv2.imread("Background2.png")
+COL_NAMES = ['NAME', 'TIME']
 
 while True:
     ret,frame=video.read()
@@ -24,14 +29,30 @@ while True:
         crop_img=frame[y:y+h, x:x+w, :]
         resized_img=cv2.resize(crop_img, (50,50)).flatten().reshape(1,-1)
         output=Knn.predict(resized_img)
+        ts=time.time()
+        date=datetime.fromtimestamp(ts).strftime("%d-%m-%y")
+        timestamp=datetime.fromtimestamp(ts).strftime("%H-%M-%S")
+        exist=os.path.isfile("Attendance/Attendance_" + date + ".csv")
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 1)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),2)
         cv2.rectangle(frame,(x,y-40),(x+w,y),(50,50,255),-1) 
         cv2.putText(frame, str(output[0]), (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
-        imgBackground[162:162 + 480, 55:55 + 640] = frame
-    cv2.imshow("Frame",imgBackground)
+        attendance=[str(output[0]), str(timestamp)]
+    cv2.imshow("Frame",frame)
     k=cv2.waitKey(1)
+    if k==ord('o'):
+        if exist:
+            with open("Attendance/Attendance_" + date + ".csv", "+a") as csvfile:
+                writer=csv.writer(csvfile)
+                writer.writerow(attendance)
+            csvfile.close()
+        else:
+            with open("Attendance/Attendance_" + date + ".csv", "+a") as csvfile:
+                writer=csv.writer(csvfile)
+                writer.writerow(COL_NAMES)
+                writer.writerow(attendance)
+            csvfile.close()
     if k==ord('q'):
         break
 video.release()
